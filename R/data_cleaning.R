c3aa22aebf5370e7122bd10c2d216e07c9409eb2
Sheikh Fahad <nameer.fahad@gmail.com>
2025-07-13 02:24:29 -0400

    data cleaning complete

--- R/data_cleaning.R
+++ R/data_cleaning.R
@@ -3,12 +3,9 @@ library(janitor)
 library(purrr)
 library(glue)
 library(stringr)
+library(yaml)
 
 load_and_combine_data <- function(directory_path) {
-  
-  if (FALSE){
-    directory_path <- "data/mean_income"
-  }
   
   file_pattern <- glue(".*-Data\\.csv$")
   
@@ -20,12 +17,12 @@ load_and_combine_data <- function(directory_path) {
     
     year_extracted <- sub("Y(\\d{4})\\.*", "\\1", year_match_result)
     
-    read.csv(path, skip = 1, na.strings = c("N", "-", "(X)")) %>% 
+    read.csv(path, skip = 1, na.strings = c("N", "-", "(X)", "null")) %>% 
       clean_names() %>%
       mutate(year = as.numeric(year_extracted)) %>%
-      select(-contains(c("margin_of_error", "ratio", "unit", "percent", "x"))) %>%
       mutate(state = str_trim(str_extract(geographic_area_name, ",\\s*([^,]+)$"), side = "left"),
-             state = str_replace(state, "^,\\s*", ""))
+             state = str_replace(state, "^,\\s*", "")) %>%
+      select(-contains(c("margin_of_error", "ratio", "unit", "percent", "x", "geograph")))
       }
     )
   
@@ -58,27 +55,79 @@ load_and_combine_data <- function(directory_path) {
   yearly_data
 }
 
-dem_data <- load_and_combine_data("data/dem")
-economic_data <- load_and_combine_data("data/economic")
-mean_income_data <- load_and_combine_data("data/mean_income")
-social_char_data <- load_and_combine_data("data/social_char")
+load_borrower_data <- function(sheet, year){
+  borrower_data <- readxl::read_excel("data/Student-loan-update-2025-Mangrum.xlsx", sheet = sheet, skip = 7) %>%
+    clean_names() %>%
+    mutate(year = year)
+  
+  borrower_data
+}
+
+dem_data <- load_and_combine_data("data/dem") %>%
+  rename(race_white = estimate_race_total_population_one_race_white,
+         race_black = estimate_race_total_population_one_race_black_or_african_american,
+         race_asian = estimate_race_total_population_one_race_asian,
+         race_native = estimate_race_total_population_one_race_american_indian_and_alaska_native,
+         race_pacific_islander = estimate_race_total_population_one_race_native_hawaiian_and_other_pacific_islander,
+         race_other = estimate_race_total_population_one_race_some_other_race,
+         race_two_or_more = estimate_race_total_population_two_or_more_races,
+         hispanic_or_latino = estimate_hispanic_or_latino_and_race_total_population_hispanic_or_latino_of_any_race,
+         population_18_or_over_male = estimate_citizen_voting_age_population_citizen_18_and_over_population_male,
+         population_18_or_over_female = estimate_citizen_voting_age_population_citizen_18_and_over_population_female) %>%
+  select(-contains("estimate")) %>%
+  group_by(state, year) %>%
+  summarise(across(everything(), \(x) sum(x, na.rm=TRUE)))
+
+economic_data <- load_and_combine_data("data/economic") %>%
+  rename(employement_total_employed = estimate_employment_status_population_16_years_and_over_in_labor_force,
+         employment_total_unemployment = estimate_employment_status_population_16_years_and_over_not_in_labor_force) %>%
+  select(-contains("estimate"))  %>%
+  group_by(state, year) %>%
+  summarise(across(everything(), \(x) sum(x, na.rm=TRUE)))
+
+mean_income_data <- load_and_combine_data("data/mean_income") %>%
+  group_by(state, year) %>%
+  summarise(mean_household_income_dollars = mean(estimate_mean_income_dollars_household_income_all_households))
+
+social_char_data <- load_and_combine_data("data/social_char") %>%
+  rename(average_family_size = estimate_households_by_type_total_households_average_family_size,
+         total_veteran = estimate_veteran_status_civilian_population_18_years_and_over,
+         total_population_bachelors_degree = estimate_educational_attainment_population_25_years_and_over_bachelor_s_degree_or_higher,
+         total_foreign_born_pop = estimate_u_s_citizenship_status_foreign_born_population,
+         total_household_w_internet_and_computer = estimate_computers_and_internet_use_total_households) %>%
+  select(-contains("estimate")) %>%
+  group_by(state, year) %>%
+  summarise(across(-average_family_size, \(x) sum(x, na.rm=TRUE)), average_family_size = mean(average_family_size))
+
+borrower_data_2019 <- load_borrower_data(11, 2019)
+borrower_data_2020 <- load_borrower_data(12, 2020)
+borrower_data_2021 <- load_borrower_data(13, 2021)
+borrower_data_2022 <- load_borrower_data(14, 2022)
+borrower_data_2023 <- load_borrower_data(15, 2023)
+
+borrower_data <- bind_rows(borrower_data_2019, borrower_data_2020, borrower_data_2021, borrower_data_2022, borrower_data_2023) %>%
+  select(state, year, total_borrowers, total_balance_billions)
 
 combined_data <- dem_data %>%
-  left_join(economic_data, by = c("geography", "geographic_area_name", "state", "year")) %>%
-  left_join(mean_income_data, by = c("geography", "geographic_area_name", "state", "year")) %>%
+  left_join(economic_data, by = c("state", "year")) %>%
+  # left_join(mean_income_data, by = c("state", "year")) %>%
   left_join(social_char_data, by = c("geography", "geographic_area_name", "state", "year")) %>%
+  select(-contains("geograph")) %>%
   relocate(state, year) %>%
-  select(-geography, -geographic_area_name, -estimate_mean_income_dollars_household_income_all_households_with_cash_public_assistance_income_or_food_stamps_snap)  %>%
   mutate(across(-c(state, year), as.numeric))
 
 cleaned_data <- combined_data %>%
   group_by(state, year) %>%
-  summarise(across(everything(), \(x) sum(x))) %>%
-  ungroup()
+  summarise(across(everything(), \(x) sum(x, na.rm=TRUE))) %>%
+  ungroup() %>%
+  left_join(mean_income_data, by = c("state", "year")) %>%
+  left_join(borrower_data, by = c("state", "year"))
+
 
 cleaned_data %>%
   select(-state, -year) %>%
   gtsummary::tbl_summary()
 
 write.csv(cleaned_data, "data/cleaned_data.csv")
+
 
